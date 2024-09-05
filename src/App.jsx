2e00645691ee7247e835999be2c9a45ea8c0945c
Jaideep Singh Thakur <jaideepsinghthakur@Jaideeps-MacBook-Pro.local>
2024-09-05 19:19:43 +0530

    added transtion

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='container'>
+         <div className='cane'>
+          <div className='pack'></div>
+          </div>
+          </div>    
+    </>
+  )
+}
+
+export default App
